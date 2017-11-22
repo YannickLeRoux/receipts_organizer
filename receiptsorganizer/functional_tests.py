7c37d2b290814d563f6be6d502ce5e3865a41c7f
YannickLeRoux <rasyann@hotmail.com>
2017-11-22 16:34:21 -0500

    First App Created

--- receiptsorganizer/functional_tests.py
+++ receiptsorganizer/functional_tests.py
@@ -1,7 +1,27 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
 
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_display_website(self):  
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('Receipts', self.browser.title)  
+        self.fail('Finish the test!')  
+
+        # She is invited to enter a to-do item straight away
+        # [...rest of comments as before]
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
---
+++ receiptsorganizer/receipts/tests.py
@@ -0,0 +1,27 @@
+from django.urls import resolve, reverse
+from django.test import TestCase, RequestFactory
+from django.http import HttpRequest
+
+from .views import HomePageView
+
+
+class HomePageTest(TestCase):
+
+    def setUp(self):
+        url=reverse('home')
+        self.response = self.client.get(url)
+
+    def test_root_url_resolves_to_HomePageView(self):
+        view = resolve('/')
+        self.assertEquals(view.func.view_class, HomePageView)
+
+    def test_home_page_returns_correct_html(self):
+        self.assertEquals(self.response.status_code, 200)
+
+        # html = self.response.content.decode('utf-8')
+        # self.assertTrue(html.startswith('<html>'))
+        # self.assertTrue(html.endswith('</html>'))
+        self.assertContains(self.response,'<html>')
+        self.assertContains(self.response,'<title>Receipts Organizer</title>')
+        
+# Create your tests here.
