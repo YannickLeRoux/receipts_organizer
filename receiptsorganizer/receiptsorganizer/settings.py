b88bed8691dee2aea89a0afa4cb4a612bab98834
YannickLeRoux <rasyann@hotmail.com>
2018-02-11 18:45:16 -0500

    Setting Up for Heroku

--- receiptsorganizer/receiptsorganizer/settings.py
+++ receiptsorganizer/receiptsorganizer/settings.py
@@ -28,9 +28,9 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ['MY_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://receipts-organizer.herokuapp.com']
 
 
 # Application definition
