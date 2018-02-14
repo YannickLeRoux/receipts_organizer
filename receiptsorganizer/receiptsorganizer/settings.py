9101bdbdac99a2472a63c75474ea362e3f4cd066
YannickLeRoux <rasyann@hotmail.com>
2018-02-13 20:41:04 -0500

    Heroku config

--- receiptsorganizer/receiptsorganizer/settings.py
+++ receiptsorganizer/receiptsorganizer/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1','https://receipts-organizer.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','receipts-organizer.herokuapp.com']
 
 
 # Application definition
